e36f829bfd08150696e795247f926e0c27bbc131
sammwadhwani138 <84030650+sammwadhwani138@users.noreply.github.com>
2024-04-18 12:58:10 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "my-azure-demo"
-    storage_account_name = "skdemosgv2"
+    storage_account_name = "skdemxxxxxx"
     container_name       = "sgterraform"
     key                  = "terraform.tfstate"
   }
